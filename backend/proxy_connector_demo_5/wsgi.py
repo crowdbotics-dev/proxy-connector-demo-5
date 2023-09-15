e2c2e2161d82b41b8f92bfb1baa3d0689c20d310
crowdbotics-dev <engineering@crowdbotics.com>
2023-09-15 15:09:46 +0000

    Update connectors

--- backend/proxy_connector_demo_5/wsgi.py
+++ backend/proxy_connector_demo_5/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proxy_connector_demo_5.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proxy_connector_demo_5.settings")
 
 application = get_wsgi_application()
